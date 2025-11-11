c566fd4131b27567d44de75da11045f97093c204
Gagan Dhanapune <135752329+gagan8605@users.noreply.github.com>
2025-11-11 17:11:37 +0530

    Add files via upload

---
+++ Day 8/Day 8.sql
@@ -0,0 +1,4 @@
+use hospital ;
+
+-- Create a patient summary that shows patient_id, full name in uppercase, service in lowercase, age category (if age >= 65 then 'Senior', if age >= 18 then 'Adult', else 'Minor'), and name length. 
+-- Only show patients whose name length is greater than 10 characters.
